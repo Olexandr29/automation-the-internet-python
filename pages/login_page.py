e62a2f39550534b24495c5e4e1e1a67ffe71306f
Olexandr Konoval <name.olexandr.konoval@gmail.com>
2026-06-16 10:02:29 +0300

    Add second test

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,7 +8,8 @@ class LoginPage:
         self.locators = {
             "username" : (By.ID, "username"),
             "password" : (By.ID, "password"),
-            "login_button" : (By.CLASS_NAME, "radius")
+            "login_button" : (By.CLASS_NAME, "radius"),
+            "alert" : (By.ID, "flash")
         }
 
     def successful_login(self, name, pas):
@@ -20,4 +21,17 @@ class LoginPage:
         username_el.send_keys(name)
         login_btn_el.click()
         return SecurePage(self.driver)
+    
+    def unsuccessful_login(self, name, pas):
+        username_el = self.driver.find_element(*self.locators["username"])
+        password_el = self.driver.find_element(*self.locators["password"])
+        login_btn_el = self.driver.find_element(*self.locators["login_button"])
+        if name == "" and pas == "" :
+            print("the username and password empty")
+        login_btn_el.click()
+        alert_el = self.driver.find_element(*self.locators["alert"])
+        alert_msg = alert_el.text
+        print(f"the alert message is '{alert_msg}'")
+        return alert_msg
+
 
--- tests/test_login.py
+++ tests/test_login.py
@@ -5,25 +5,36 @@ from pages.secure_page import SecurePage
 
 class TestLogin:
 
-    def test_login(self):
+    def setup_method(self, method):
+        test_name = method.__name__
+        print(f"==========-=========The {test_name} is started==========-=========")
         self.driver = webdriver.Chrome()
         self.home_page = HomePage(self.driver)
         self.driver.get(self.home_page.URL)
-        print(f"The Home page {self.home_page.URL} is opened")
         self.login_page = self.home_page.open_login_page()
+
+    def test_1_Successful_login(self):
         assert self.driver.current_url == self.login_page.URL
-        print(f"The Login page {self.login_page.URL} is opened")
-        self.secure_page = self.login_page.successful_login("tomsmith", "SuperSecretPassword!")
-        assert self.driver.current_url == self.secure_page.URL
-        print(f"The Secure page {self.secure_page.URL} is opened")
+        secure_page = self.login_page.successful_login("tomsmith", "SuperSecretPassword!")
         expected_alert_msg = "You logged into a secure area!"
-        assert expected_alert_msg in self.secure_page.get_alert_message(), \
+        assert expected_alert_msg in secure_page.get_alert_message(), \
         "the alert message is wrong"
         expected_welcome_msg = "Welcome to the Secure Area. When you are done click logout below."
-        assert expected_welcome_msg in self.secure_page.get_welcome_message(), \
+        assert expected_welcome_msg in secure_page.get_welcome_message(), \
         "the welcome message is wrong"
-        assert self.secure_page.is_logout_button_displayed() == True, \
+        assert self.driver.current_url == secure_page.URL
+        assert secure_page.is_logout_button_displayed() == True, \
         "the logout button is not displayed"
-        self.driver.quit()
+
+    def test_2_Unsuccessful_login_with_empty_credentials(self):
+        expected_alert_msg = "Your username is invalid!"
+        actual_alert_msg = self.login_page.unsuccessful_login("", "")
+        assert expected_alert_msg in actual_alert_msg
+        assert self.driver.current_url == self.login_page.URL
+        
+
+    def teardown_method(self):    
+        self.driver.quit() 
+    
 
         
