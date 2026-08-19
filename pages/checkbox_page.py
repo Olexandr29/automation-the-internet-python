07f6e5176cf7648f83401e5235c8c85bda79b9ae
Olexandr Konoval <name.olexandr.konoval@gmail.com>
2026-08-19 11:45:05 +0300

    feat: add tests for TC32-35

--- pages/checkbox_page.py
+++ pages/checkbox_page.py
@@ -1,5 +1,7 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 class CheckboxPage(BasePage):
     def __init__(self, driver, timeout=10):
@@ -13,5 +15,36 @@ class CheckboxPage(BasePage):
         checkboxes = self.driver.find_elements(*self.locators["checkboxes"])
         if checkbox_number > len(checkboxes):
             return False
-    
         return checkboxes[checkbox_number - 1].is_displayed()
+
+    def is_checkbox_checked(self, checkbox_number):
+        checkboxes = self.driver.find_elements(*self.locators["checkboxes"])
+        return checkboxes[checkbox_number - 1].is_selected()
+
+    def check_specific_checkbox(self, checkbox_number):
+        checkboxes = self.driver.find_elements(*self.locators["checkboxes"])
+        return checkboxes[checkbox_number - 1].click()
+
+    # @Reporter.step("Focus the checkbox")
+    def focus_checkbox(self, checkbox_number):
+        actions = ActionChains(self.driver)
+        checkboxes = self.driver.find_elements(*self.locators["checkboxes"])
+        target_checkbox = checkboxes[checkbox_number - 1]
+
+        for _ in range(10):
+            active_element = self.driver.switch_to.active_element
+            if active_element == target_checkbox:
+                return 
+            actions.send_keys(Keys.TAB).perform()
+   
+    def is_checkbox_focused(self, checkbox_number):
+        focused = self.driver.switch_to.active_element
+        checkboxes = self.driver.find_elements(*self.locators["checkboxes"])
+        return focused == checkboxes[checkbox_number - 1]
+
+    def press_space(self, checkbox_number):
+        checkboxes = self.driver.find_elements(*self.locators["checkboxes"])
+        checkboxes[checkbox_number - 1].send_keys(Keys.SPACE)
+
+        
+       
--- tests/test_checkbox.py
+++ tests/test_checkbox.py
@@ -9,8 +9,39 @@ class TestCheckbox(BaseTest):
     def setup_checkbox_page(self, setup_test):
         self.checkbox_page = self.home_page.open_checkbox_page()
 
-    def test_31_verify_Checkboxes_visible(self):
-        assert self.driver.current_url == CheckboxData.URL_CHECKBOX_PAGE
-        assert self.checkbox_page.is_checkbox_visible(1) == True
-        assert self.checkbox_page.is_checkbox_visible(2) == True
+    def test_31_verify_checkboxes_visible(self):
+        assert self.driver.current_url == CheckboxData.URL_CHECKBOX_PAGE, "The Checkboxes page is not opened"
+        assert self.checkbox_page.is_checkbox_visible(1) == True, f"The {CheckboxData.CHECKBOX_1} is not visible"
+        assert self.checkbox_page.is_checkbox_visible(2) == True, f"The {CheckboxData.CHECKBOX_2} is not visible"
 
+    def test_32_verify_checkboxes_initial_state(self):
+        assert self.checkbox_page.is_checkbox_checked(1) == False, f"The {CheckboxData.CHECKBOX_1} is not unchecked"
+        assert self.checkbox_page.is_checkbox_checked(2) == True, f"The {CheckboxData.CHECKBOX_2} is not checked"
+
+    def test_33_verify_checkboxes_state_changes_correctly(self):
+        self.checkbox_page.check_specific_checkbox(1)
+        assert self.checkbox_page.is_checkbox_checked(1) == True, f"The {CheckboxData.CHECKBOX_1} is not checked after first click"
+        self.checkbox_page.check_specific_checkbox(1)
+        assert self.checkbox_page.is_checkbox_checked(1) == False, f"The {CheckboxData.CHECKBOX_1} is not unchecked after second click"
+        self.checkbox_page.check_specific_checkbox(2)
+        assert self.checkbox_page.is_checkbox_checked(2) == False, f"The {CheckboxData.CHECKBOX_2} is not unchecked after first click"
+        self.checkbox_page.check_specific_checkbox(2)
+        assert self.checkbox_page.is_checkbox_checked(2) == True, f"The {CheckboxData.CHECKBOX_2} is checked after second click"
+
+    def test_34_verify_checkboxes_state_after_refresh(self):
+        self.checkbox_page.check_specific_checkbox(1)
+        assert self.checkbox_page.is_checkbox_checked(1) == True, f"The {CheckboxData.CHECKBOX_1} is not checked"
+        assert self.checkbox_page.is_checkbox_checked(2) == True, f"The {CheckboxData.CHECKBOX_2} is not checked"
+        self.driver.refresh()
+        assert self.checkbox_page.is_checkbox_checked(1) == False, f"The {CheckboxData.CHECKBOX_1} is checked"
+        assert self.checkbox_page.is_checkbox_checked(2) == True, f"The {CheckboxData.CHECKBOX_2} is not checked"
+
+    def test_35_verify_checkbox_state_chnges_using_keyboard(self):
+        self.checkbox_page.focus_checkbox(1)
+        assert self.checkbox_page.is_checkbox_focused(1) == True, f"The {CheckboxData.CHECKBOX_1} is not focused"
+        self.checkbox_page.press_space(1)
+        assert self.checkbox_page.is_checkbox_checked(1) == True, f"The {CheckboxData.CHECKBOX_1} state is not chaged via pressing space key"
+        self.checkbox_page.focus_checkbox(2)
+        assert self.checkbox_page.is_checkbox_focused(2) == True, f"The {CheckboxData.CHECKBOX_2} is not focused"
+        self.checkbox_page.press_space(2)
+        assert self.checkbox_page.is_checkbox_checked(2) == False, f"The {CheckboxData.CHECKBOX_2} state is not chaged via pressing space key"
