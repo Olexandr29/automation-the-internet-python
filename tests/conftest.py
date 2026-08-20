0dcf4a51d833203cd94ccf9a8fd57fb48268bf11
Olexandr Konoval <name.olexandr.konoval@gmail.com>
2026-08-20 17:19:18 +0300

    refactor: remove redundant screenshot code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,4 @@
 import allure
-import base64
-from allure_commons.types import AttachmentType
 import pytest
 
 @pytest.hookimpl(hookwrapper=True)
@@ -18,16 +16,3 @@ def pytest_runtest_makereport(item, call):
                 name="Screenshot on failure",
                 attachment_type=allure.attachment_type.PNG,
             )
-
-# def pytest_selenium_capture_debug(item, report, extra):
-#     for log_type in extra:
-#         if log_type["name"] == "Screenshot":
-#             content = base64.b64decode(
-#                 log_type["content"].encode("utf-8")
-#                 )
-#             allure.attach(
-#                 content,
-#                 name="Screenshot on failure",
-#         attachment_type=AttachmentType.PNG,
-#     )
-
